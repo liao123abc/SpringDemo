6ba32db69ca508ac135e4dd2217ed0b03457748d
ThomasLiao <liao123abc@yeah.net>
2017-06-26 23:17:24 +0800

    Update todo list

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * todo 使用builder模式针对保利视频进行重构和测试，写一篇博文介绍builder模式
+ * 1.常用的ui组件使用builder模式
+ * 2.使用builder模式封装好httpclient来进行操作
+ */
 @SpringBootApplication
 public class DemoApplication {
 
